60ef4f5d07b55fa326157a7380786c2a987963b1
HarshithaDinesh <harshithadinesh97@gmail.com>
2022-07-08 13:44:44 +0530

    init

---
+++ src/api/index.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+// Enables client side protection against cross-site request forgery
+
+const url = 'http://localhost:5000/posts'
+
+export const fetchPosts =() => axios.get(url);
+
+export const createPost = (newPost) => axios.post(url, newPost)
+
+export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost);
+
+export const deletePost = (id) => axios.delete(`${url}/${id}`);
+
+export const likePost = (id) =>axios.patch(`${url}/${id}/likePost`);
---
+++ src/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from "redux";
+import posts from './posts'
+
+export default combineReducers({ posts })
+// This posts is imported in Posts/posts.js
